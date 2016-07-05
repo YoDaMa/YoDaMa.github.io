e268f9c138112ee5a67804f87a672fe15684aa0e
YoDaMa <yoseph.maguire@gmail.com>
2016-07-05 11:15:35 -0500

    pls

--- js/grayscale.js
+++ js/grayscale.js
@@ -178,7 +178,7 @@ function init() {
 
     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
     var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
+    var myLatLng = new google.maps.LatLng(29.7147735,-95.4020761);
     var beachMarker = new google.maps.Marker({
         position: myLatLng,
         map: map,
